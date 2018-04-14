f6ffe0573d1383cbadc49ac282295bee799decb8
Daniel Bliss <dbliss@Daniels-MacBook-Pro.local>
2018-04-14 15:25:15 -0500

    slicing lists and strings

---
+++ slicing lists and strings.py
@@ -0,0 +1,21 @@
+
+my_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+#          0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+#        -10,-9,-8,-7,-6,-5,-4,-3,-2,-1
+
+# list[start:end:step]
+
+print my_list[::-1]
+
+
+
+
+
+
+
+
+
+
+
+sample_url = 'http://coreyms.com'
+print sample_url[7:-4]
